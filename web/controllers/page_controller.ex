d85bd28556ee51456903340ce1cbbec87ec514b1
kstrempel <kstrempel@gmail.com>
2015-09-24 21:13:26 +0200

    add login and create prototype

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,20 @@ defmodule Grbio.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def login(conn, _params) do
+    render conn, "login.html"
+  end
+
+  def create(conn, _params) do
+    render conn, "create.html"
+  end
+
+  def postlogin(conn, _params) do
+    render conn, "successfull.html"
+  end
+
+  def postcreate(conn, _params) do
+    render conn, "successfull.html"
+  end
 end
